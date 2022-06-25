627c62a8cbbde623f44f32e4631707316b049702
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-06-26 01:07:55 +0530

    Button Components

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div>
       <h1>Hello From React App</h1>
+      <div>
+        <Button text="Submit" path="https://reactjs.org/" /> <br/> <br/>
+
+        <Button text="Sign In" path="https://reactjs.org/" /> <br/> <br/>
+
+        <Button text="Sign Up" path="https://reactjs.org/" />
+      </div>
     </div>
   );
 }
---
+++ react-intro/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import style from '../style/Button.module.css';
+// import Link from 'react-dom';
+
+const Button = ({ text, path }) => {
+  return (
+    <>
+      <button className={style.button_comp}>
+        {text} 
+      </button>
+    </>
+  );
+};
+export default Button;
