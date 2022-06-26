bcd8faee5e180a6afc2e15cc4cc92eb9e729da52
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-06-26 22:51:23 +0530

    Card component

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import Button from './components/Button';
+import Card from './components/Card';
 
 function App() {
+  const carddata = [{
+    src :'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg',
+    head: 'Lorem ipsum dolor sit amet amet consectetur.',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit adipisicing elit. Provident esse, eveniet quam quis unde '
+  }];
+ 
   return (
     <div>
       <h1>Hello From React App</h1>
       <div>
-        <Button text="Submit" path="https://reactjs.org/" /> <br/> <br/>
+        <Button text="Submit" path="https://reactjs.org/" /> <br/> 
 
-        <Button text="Sign In" path="https://reactjs.org/" /> <br/> <br/>
+        <Button text="Sign In" path="https://reactjs.org/" /> <br/> 
 
         <Button text="Sign Up" path="https://reactjs.org/" />
       </div>
+
+      <div>
+        {
+          carddata.map(e =>{
+            return <Card src={e.src} head={e.head} desc={e.desc}/>
+          })
+        }
+      </div>
+
     </div>
   );
 }
---
+++ react-intro/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import style from '../style/Card.module.css';
+const Card = ({ src, head, desc }) => {
+  return (
+    <>
+      <div className={style.card}>
+        <div className={style.card_img_div}>
+          <img src={src} alt="divimage" className={style.card_img}/>
+        </div>
+        <div className={style.text_div}>
+          <h3 className={style.head}>{head}</h3>
+          <p className={style.desc}>{desc}</p>
+          <a href="/" className={style.card_link}>Read More...</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
+
+
