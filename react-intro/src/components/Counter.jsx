8d617ff4c06ba6300f0e2ca61070231524ed56ba
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-07-03 23:57:04 +0530

    Counter Component working

--- react-intro/src/components/Counter.jsx
+++ react-intro/src/components/Counter.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Counter() {
+
+  const [count, setCount] = useState(0);
+  const changeHandller = (e) =>{
+    setCount(count+e);
+  }
   return (
     <div>
-      <h1>Counter</h1>
+      <h1>Counter : {count}</h1>
+      <div>
+        <button onClick={()=>changeHandller(1)}>Add 1</button>
+        <button onClick={()=>changeHandller(-1)}>Dec 1</button>
+      </div>
     </div>
   )
 }
